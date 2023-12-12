4c5ec5d198b16231c108b04a3d0386748ef202f2
mr Rosas <info@alfcomputacion.com>
2023-12-11 17:34:26 -0800

    get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,26 +13,52 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   
+  return res.send(JSON.stringify(books, null, 4 ));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  let book = Object.values(books)[isbn]
+  if(book){
+    return res.status(200).send(JSON.stringify(book))
+  }else{
+    return res.status(400).json({message: "ISBN not found"});
+  }
+  // console.log(filtered)
+  // let filtered = books.find(book)
+  // return res.send(JSON.stringify(filtered, null, 4))
+  // return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = Object.values(books).filter(el => {
+    return el.author === req.params.author
+  })
+  if(author.length > 0){
+      return res.send(JSON.stringify(author))
+  }else{
+      return res.status(400).json({message: "Author not found"});
+
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  let title = Object.values(books).filter(el => {
+    return el.title === req.params.title
+  })
+  if(title.length > 0){
+      return res.send(JSON.stringify(title))
+  }else{
+      return res.status(400).json({message: "Author not found"});
+
+  }});
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
