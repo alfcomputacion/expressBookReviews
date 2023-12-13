543fb12fe6846b5fdbaf0ef9531dc1c5716c942d
mr Rosas <info@alfcomputacion.com>
2023-12-12 17:31:14 -0800

    working on login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
@@ -7,16 +8,45 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let userswithsmaename = users.filter(user =>{
+    return user.username === username
+});
+if(userswithsmaename.length > 0){
+    return true;
+}else{
+    return false;
+}
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validusers = users.filter(user =>{
+    return (user.username === username && user.password === password)
+});
+if(validusers.length >0){
+    return true;
+}else{
+    return false;
+}
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username
+  const password = req.body.password
+  if(!username || !password){
+      return res.status(400).json({message: "Error logging in!"});
+  }
+  if(authenticatedUser(username, password)){
+    let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send("User successfully logged in");
+  }else{
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
