9270b46a83db21d32c93d91c507b6eadd2fef263
mr Rosas <info@alfcomputacion.com>
2023-12-13 17:35:19 -0800

    task 9 working on it

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,7 +4,10 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
+// let users = [{"username": "alf", "password": "123","username": "joe", "password": "123"}];
 let users = [];
+
+
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -52,7 +55,41 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn
+    let review = req.query.review
+    let username = req.session.authorization.username;
+    let reviewName = books[isbn]["reviews"]["name"]
+    let obj = books[isbn]["reviews"]
+
+    if(books[isbn]["reviews"][username]){
+      books[isbn]["reviews"]={[username]:review}
+      return res.status(300).json({message: "Review same user saved " + JSON.stringify(books[isbn])});
+  }else{
+      // books[isbn]["reviews"]={[username]:review}
+      Object.defineProperty(books[isbn]["reviews"], [username], {
+          value: review,
+          writable: false,
+          enumerable: true,
+          configurable: true,
+        });
+
+      return res.status(300).json({message: "Different user Review saved " + JSON.stringify(books[isbn])});
+  }
+  // if (!Object.keys(obj).length > 0){
+  // }else{
+  //     return true
+  // }
+  // if(reviewName){
+  //     if(books[isbn]["reviews"]["name"] === username){
+  //         books[isbn]["reviews"]={...books[isbn]["reviews"],"name": username, "review": review}
+  //     }else{
+  //         books[isbn]["reviews"]={...books[isbn]["reviews"],"name": username, "review": review}
+  //     }
+  // }else{
+  // }
+//   let review = req.body.review
+//   let isbn = req.params.isbn
+  // let review = Object.values(books)[isbn]
 });
 
 module.exports.authenticated = regd_users;
